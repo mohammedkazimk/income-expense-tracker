58332cb54d6c05f88aa0f5622961640c17a002f4
mohammedkazimk <mohammedkazimk3@gmail.com>
2026-07-16 14:14:30 +0530

    Modify Not Found Page Text

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div className="m-5"><span className="font-bold">Oops.</span> Return to Home Page <Link to="/" className="text-blue-500 hover:underline">Click here</Link></div>
+    <div className="m-5"><span className="font-bold">Oops.</span> Page Not Found. Return to Home Page <Link to="/" className="text-blue-500 hover:underline">Click here</Link></div>
   )
 }
 
